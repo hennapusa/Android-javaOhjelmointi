4f81954952e6b0918f55711a9ad032ca6c808819
hennapusa <Henna.Pusa@student.lab.fi>
2022-09-01 10:26:59 +0300

    ensimmäinen tehtävä

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button welcomeButton;
+    private View welcomeTextView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        welcomeButton = findViewById(R.id.hello_button);
+        welcomeTextView =findViewById(R.id.textView);
+        welcomeButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                if (welcomeTextView.getVisibility() == View.VISIBLE)
+                    welcomeTextView.setVisibility(View.INVISIBLE);
+
+                else if(welcomeTextView.getVisibility() == View.INVISIBLE)
+                welcomeTextView.setVisibility(View.VISIBLE);
+
+
+                }
+
+        });
     }
 }
