8eeb04418448bc42f5d873a402a5a90d31243cbb
hennapusa <Henna.Pusa@student.lab.fi>
2022-09-14 13:45:11 +0300

    peli korjattu

--- app/src/main/java/com/example/myapplication/Guess.java
+++ app/src/main/java/com/example/myapplication/Guess.java
@@ -1,15 +1,31 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
-public class Guess extends AppCompatActivity {
 
+import java.util.Random;
+public class Guess extends AppCompatActivity {
+    private Button Button1;
+    private Button Button2;
+    private Button Button3;
+    private Button Button4;
+
+    public class generateRandom {
+
+        public void main(String args[]) {
+            //satunnaisluku?
+            Random rand = new Random();
+            int rand_int1 = rand.nextInt(5);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guess);
 
-            }
-        }
+
+    }
+}
