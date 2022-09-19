5b5b6776e0fa3b24ae25a9499f4e6109a394b9f6
hennapusa <Henna.Pusa@student.lab.fi>
2022-09-19 08:49:03 +0300

    animaatio ja tähti lisätty

--- app/src/main/java/com/example/myapplication/Guess.java
+++ app/src/main/java/com/example/myapplication/Guess.java
@@ -1,10 +1,12 @@
 package com.example.myapplication;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -22,12 +24,13 @@ public class Guess extends AppCompatActivity {
     Animation animation;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guess);
 
-         animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.animation1);
+
 
         game_Button1 = findViewById(R.id.imageButton1);
         game_Button1.setOnClickListener(new View.OnClickListener() {
@@ -70,8 +73,12 @@ public class Guess extends AppCompatActivity {
 
         });
 
+        animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.animation1);
+        ImageView image = (ImageView) findViewById(R.id.imageButton1);
+        image.startAnimation(animation);
+
         Random rand = new Random();
-        rand_int1 = rand.nextInt(5);
+        rand_int1 = rand.nextInt(3)+1;
 
     }
 
@@ -79,23 +86,44 @@ public class Guess extends AppCompatActivity {
         switch (view.getId()) {
             case R.id.imageButton1:
                 game_Button1.setVisibility(View.INVISIBLE);
-                game_Button1.startAnimation(animation);
+                game_Button1.startAnimation((animation));
+                if(rand_int1 ==1){
+                    game_Button1.setImageResource(R.mipmap.star);
+                    game_Button1.setBackgroundColor(Color.YELLOW);
+                    game_Button1.setVisibility(View.VISIBLE);
+                }
+
                 break;
 
             case R.id.imageButton2:
                 game_Button2.setVisibility(View.INVISIBLE);
-                game_Button2.startAnimation(animation);
+                game_Button2.startAnimation((animation));
+                if(rand_int1 ==2){
+                    game_Button2.setImageResource(R.mipmap.star);
+                    game_Button2.setBackgroundColor(Color.YELLOW);
+                    game_Button2.setVisibility(View.VISIBLE);
+                }
 
                 break;
 
             case R.id.imageButton3:
                 game_Button3.setVisibility(View.INVISIBLE);
-                game_Button3.startAnimation(animation);
+                game_Button3.startAnimation((animation));
+                if(rand_int1 ==3){
+                    game_Button3.setImageResource(R.mipmap.star);
+                    game_Button3.setBackgroundColor(Color.YELLOW);
+                    game_Button3.setVisibility(View.VISIBLE);
+                }
                 break;
 
             case R.id.imageButton4:
                 game_Button4.setVisibility(View.INVISIBLE);
-                game_Button4.startAnimation(animation);
+                game_Button4.startAnimation((animation));
+                if(rand_int1 ==4){
+                    game_Button4.setImageResource(R.mipmap.star);
+                    game_Button4.setBackgroundColor(Color.YELLOW);
+                    game_Button4.setVisibility(View.VISIBLE);
+                }
                 break;
 
             case R.id.restart_btn:
