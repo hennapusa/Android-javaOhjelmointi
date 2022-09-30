449795fe4dc340a55a118ce616f45fd2ccdbed87
hennapusa <Henna.Pusa@student.lab.fi>
2022-09-30 14:24:41 +0300

    ajastin!!

--- app/src/main/java/com/example/myapplication/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/myapplication/ui/notifications/NotificationsFragment.java
@@ -1,37 +1,91 @@
 package com.example.myapplication.ui.notifications;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.NumberPicker;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.myapplication.R;
 import com.example.myapplication.databinding.FragmentNotificationsBinding;
+import com.google.android.material.button.MaterialButtonToggleGroup;
 
 public class NotificationsFragment extends Fragment {
 
     private FragmentNotificationsBinding binding;
+    // private static final long START_TIME_IN_MILLIS = 60000;
+    //private Button buttonStart;
+    //private Button buttonStop;
+    //private Button buttonPause;
+    //private CountDownTimer CountDownTimer;
+    int valueOnPicker1 =0;
+    NumberPicker numPicker;
+    private MaterialButtonToggleGroup materialButtonToggleGroup;
+    public static final String TAG ="MyAppMessage";
+
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         NotificationsViewModel notificationsViewModel =
                 new ViewModelProvider(this).get(NotificationsViewModel.class);
 
+        String[] myValues = new String[61];
+        for (int i = 0; i < myValues.length; i++) {
+            myValues[i] = i+ " s";
+        }
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textNotifications;
         notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        numPicker = root.findViewById(R.id.numberpicker);
+        numPicker.setDisplayedValues(myValues);
+        numPicker.setMinValue(0);
+        numPicker.setMaxValue(60);
+        numPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker numberPicker, int i, int i1) {
+
+                valueOnPicker1 = numberPicker.getValue();
+                Log.e(TAG, "painettu" + "");
+            }
+        });
+        materialButtonToggleGroup = root.findViewById(R.id.toggleGroup);
+        materialButtonToggleGroup.addOnButtonCheckedListener(
+                new MaterialButtonToggleGroup.OnButtonCheckedListener() {
+                    @Override
+                    public void onButtonChecked(MaterialButtonToggleGroup group, int checkedId, boolean isChecked) {
+                        if (isChecked) {
+                            if (checkedId == R.id.buttonPause) {
+                                Log.e(TAG, "pausea painettu" + String.valueOf(numPicker.getValue()));
+                                //numPicker.getValue();
+
+                            } else if (checkedId == R.id.buttonStart) {
+                                Log.e(TAG, "starttia painettu"+ String.valueOf(numPicker.getValue()));
+
+                            } else if (checkedId == R.id.buttonStop) {
+                               // Log.e(TAG, "stop painettu"+ String.valueOf(numPicker.setVa));
+                                numPicker.setValue(0);
+                            }
+                        }
+                    }
+                });
         return root;
     }
+
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
     }
+
 }
